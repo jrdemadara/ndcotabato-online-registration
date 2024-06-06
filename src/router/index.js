490cc21008995f5f51f1f2e309c8d51ceca8effd
Your Name <jrdemadara@protonmail.com>
2024-06-05 22:15:58 -0700

    fix: cahnge route to WebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import ProfileView from '../views/ProfileView.vue'
@@ -6,7 +6,7 @@ import ServerErrorView from '../views/errors/ServerError.vue'
 import PasswordResetView from '../views/PasswordResetView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
